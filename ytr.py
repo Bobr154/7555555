42d9678e00f7df54d9f249c3b118cd6636d5ebb2
Beaver 123 <kepkak79@gmail.com>
2024-02-04 14:51:13 +0300

    ytr commit

---
+++ ytr.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+window = tk.Tk()
+
+gg = tk.Label(
+    text="000000, idjdsk",
+    fg='#1eff00',
+    bg ='#a13939',
+    width=10,
+    height=10)
+
+gg.pack()
+
+window.mainloop()
+
+
+
